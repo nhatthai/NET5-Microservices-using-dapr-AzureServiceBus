5940c7fe5e570563a399c17dd251d750993256a7
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2021-12-26 09:37:09 +0700

    Add Consumer

---
+++ DemoMicroservices/Messages/Commands/Notification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Messages.Commands
+{
+    public class Notification
+    {
+        public Guid NotificationId { get; set; }
+
+        public string NotificationType { get; set; }
+
+        public string NotificationContent { get; set; }
+
+        public string NotificationAddress { get; set; }
+
+        public DateTime NotificationDate { get; set; }
+    }
+}
