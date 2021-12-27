bd97d76ee53292b01f720099e992ea3e93657841
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2021-12-27 13:28:23 +0700

    Update Consumer Controller

---
+++ DemoMicroservices/Consumer/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Consumer.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        [Route("/home")]
+        public void GetHome()
+        {
+            Console.WriteLine("GET Home API");
+            _logger.LogInformation("GET Home API");
+        }
+    }
+}
--- DemoMicroservices/Consumer/Controllers/ProcessorController.cs
+++ DemoMicroservices/Consumer/Controllers/ProcessorController.cs
@@ -3,27 +3,27 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Dapr;
 
-namespace CheckoutService.controller
+namespace Consumer.Controllers
 {
     [ApiController]
-    public class CheckoutServiceController : ControllerBase
+    public class ProcessorController : ControllerBase
     {
-        private readonly ILogger<CheckoutServiceController> _logger;
+        private readonly ILogger<ProcessorController> _logger;
         private const string DAPR_PUBSUB_NAME = "orderpubsub";
         private const string TOPIC_NAME = "orders";
 
-        public CheckoutServiceController(ILogger<CheckoutServiceController> logger)
+        public ProcessorController(ILogger<ProcessorController> logger)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         //Subscribe to a topic
         [Topic(DAPR_PUBSUB_NAME, TOPIC_NAME)]
-        [HttpPost("checkout")]
-        public void Checkout([FromBody] Messages.Commands.Order order)
+        [HttpPost("processor")]
+        public void Processor([FromBody] Messages.Commands.Order order)
         {
             Console.WriteLine("Subscriber received : " + order.OrderId);
-            _logger.LogInformation("Received checkout: {OrderId}, {OrderNumber}", order.OrderId, order.OrderNumber);
+            _logger.LogInformation("Received Order: {OrderId}, {OrderNumber} for Processor", order.OrderId, order.OrderNumber);
         }
     }
 }
